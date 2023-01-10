a9712a4a07e64e3acaed74f0597f4b3205064e3e
factscub <factscub@gmail.com>
2023-01-10 18:25:51 +0530

    overflow

--- frontend/src/Components/BlackBtn/BlackBtn.style.js
+++ frontend/src/Components/BlackBtn/BlackBtn.style.js
@@ -4,10 +4,10 @@ export const Wrapper = styled.button`
 background: #270F36;
 border-radius: 8px;
 box-shadow: 2px 2px 16px rgba(0, 0, 0, 0.16);
-position: absolute;
+position: sticky;
 width: 124px;
 height: 40px;
-right: 24px;
+left: -100px;
 bottom: 30px;
 color:white;
 cursor:pointer;
